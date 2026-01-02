44ecc77fc547ae9df6ebd6a05503efa5b6d227b6
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-01-02 18:35:38 +0100

    products module

---
+++ src/models/product.ts
@@ -0,0 +1,17 @@
+import type { CategoryRead } from './category'
+
+export type ProductImportance = 'important' | 'necessary' | 'unnecessary'
+
+export type ProductCreate = {
+  name: string
+  category_id: string
+  importance: ProductImportance
+}
+
+export type ProductRead = {
+  id: string
+  name: string
+  importance: ProductImportance
+  created_at: string
+  category: CategoryRead
+}
