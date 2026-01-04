f7224e9109f144af30fe70090fa14d6bd7e064a2
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-01-04 20:13:45 +0100

    barrel export

---
+++ src/ui/index.ts
@@ -0,0 +1,22 @@
+export { cn } from './cn'
+
+export { default as Button } from './button'
+export { default as Input } from './input'
+export { default as Textarea } from './textarea'
+export { default as Select } from './select'
+export { default as Label } from './label'
+export { default as FieldError } from './field-error'
+export { default as Spinner } from './spinner'
+export { default as EmptyState } from './empty-state'
+
+export {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalTitle,
+  ModalDescription,
+  ModalFooter,
+  ModalClose,
+} from './modal'
+
+export { notify } from './toast'
