0357188f1e0fc1698f5d45e11b92e41021167058
Mikołaj Mozoluk <mikolaj.moozoluk@gmail.com>
2026-01-05 11:07:10 +0100

    added routePatterns

--- src/routes/routePaths.ts
+++ src/routes/routePaths.ts
@@ -15,9 +15,7 @@ export const routePaths = {
 
   wallets: {
     list: () => `/wallets`,
-    base: (walletId: Id) => {
-      wallet(walletId)
-    },
+    base: (walletId: Id) => wallet(walletId),
     transactions: (walletId: Id) => `${wallet(walletId)}/transactions`,
     categories: (walletId: Id) => `${wallet(walletId)}/categories`,
     products: (walletId: Id) => `${wallet(walletId)}/products`,
@@ -25,5 +23,25 @@ export const routePaths = {
     members: (walletId: Id) => `${wallet(walletId)}/members`,
     dashboard: (walletId: Id) => `${wallet(walletId)}/dashboard`,
     history: (walletId: Id) => `${wallet(walletId)}/history`,
+  },
+} as const
+
+export const routePatterns = {
+  auth: {
+    login: '/login',
+  },
+  settings: {
+    root: '/settings',
+  },
+  wallets: {
+    list: '/wallets',
+    base: '/wallets/:walletId',
+    transactions: '/wallets/:walletId/transactions',
+    categories: '/wallets/:walletId/categories',
+    products: '/wallets/:walletId/products',
+    recurring: '/wallets/:walletId/recurring',
+    members: '/wallets/:walletId/members',
+    dashboard: '/wallets/:walletId/dashboard',
+    history: '/wallets/:walletId/history',
   },
 } as const
