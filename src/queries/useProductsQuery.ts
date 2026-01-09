006f2cd9dc5e722b7c5ba9183b149fb35b1859a0
Mikołaj <mikolaj.moozoluk@gmail.com>
2026-01-09 19:17:24 +0100

    fixed useProductsQuery

--- src/queries/useProductsQuery.ts
+++ src/queries/useProductsQuery.ts
@@ -3,13 +3,14 @@ import { queryKeys } from './queryKeys'
 import { productsApi } from '@/api/modules'
 import { getTokenFromStorage } from '@/authentication/handleToken'
 
-export default function useProductsQuery(walletId: string, categoryId: string | undefined) {
+export default function useProductsQuery(walletId: string, categoryId?: string) {
   const token = getTokenFromStorage()
   const hasToken = !!token?.access_token
 
   return useQuery({
     queryKey: queryKeys.wallets.products.all(walletId, categoryId),
-    queryFn: () => productsApi.getAll(walletId, { category_id: categoryId }),
+    queryFn: () =>
+      productsApi.getAll(walletId, categoryId ? { category_id: categoryId } : undefined),
     enabled: hasToken && !!walletId,
     retry: false,
   })
